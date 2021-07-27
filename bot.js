a183dea248796bc26377c3797d38c609d80cc1a0
/MrRobot <87871568+kakr0b0t@users.noreply.github.com>
2021-07-27 13:56:34 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1590,19 +1590,3 @@ client.on("message", async message => {
     message.channel.send(embed);
   }
 });
-
-client.on('message', message => {
-     if (message.content  === prefix + "emojilist") {
-     let notAnimated = [];
-     let animated = [];
-        message.guild.emojis.cache.forEach(async emoji => {
-          if (emoji.animated) animated.push(emoji.toString());
-          else notAnimated.push(emoji.toString());
-        });
-        if (!animated[0]) animated = ['None'];
-        if (!notAnimated[0]) notAnimated = ['None'];
-        message.channel.send('**__ANIMATED  :__**\n' + animated.join(' ') + '\n**__NORMAL:__**\n' + notAnimated.join(' '), {split:true});
-      } catch (err) {
-        message.channel.send('Their was an error!\n' + err).catch();
-    }
-}); 
