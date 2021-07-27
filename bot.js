891e01b151e5ff5979df230a137c74df7510d5af
/MrRobot <87871568+kakr0b0t@users.noreply.github.com>
2021-07-27 14:07:53 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1590,19 +1590,3 @@ client.on("message", async message => {
     message.channel.send(embed);
   }
 });
-
-client.on("message", async message  => {
-if (message.content.startsWith(prefix + "botinfo")) {
-      let embed = new discord.MessageEmbed()
-      .setTitle(`__**INFORMATION ABOUT BOT**__`)
-      .addField("Bot Name", `<@${client.user.id}>`)
-      .addField("Bot Developer", `@MrRobot#7265`)
-      .addField("Total Guilds", `${client.guilds.cache.size}`, true)
-      .addField("Total Channels", `${client.channels.cache.size}`)
-      .addField("Total Users", `${client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)}`, true)
-      .addField("Bot Libary", `discord.js`)
-      .setColor("#0d2943")
-      .setFooter(`information about bot`);
-       message.channel.send(embed);
-  }
-});
